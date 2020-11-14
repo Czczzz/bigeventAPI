b5f973b62ffba97d746975c2aebeac1c3f1d1007
陈子聪 <254612409@qq.com>
2020-11-14 14:36:56 +0800

    登录注册修改

--- api-server/app.js
+++ api-server/app.js
@@ -10,12 +10,40 @@ const cors = require('cors')
 // 将cors注册为全局中间件
 app.use(cors())
 
+const joi = require('@hapi/joi')
+
+app.use(function (err, req, res, next) {
+    // 数据验证失败
+    if (err instanceof joi.ValidationError) return res.cc(err)
+    // 未知错误
+    res.cc(err)
+})
+
+// 响应数据的中间件
+app.use(function (req, res, next) {
+    // status = 0 为成功； status = 1 为失败； 默认将 status 的值设置为 1，方便处理失败的情况
+    res.cc = function (err, status = 1) {
+        res.send({
+            // 状态
+            status,
+            // 状态描述，判断 err 是 错误对象 还是 字符串
+            message: err instanceof Error ? err.message : err,
+        })
+    }
+    next()
+})
+
 // 配置解析表单数据的中间件
 app.use(express.urlencoded({ extended: false }))
 
 // 导入并注册用户路由模板
-const userRouter = require('./router/user')
+const userRouter = require('./router/user');
 app.use('/api', userRouter)
+// 导入并使用用户信息的路由模板
+const userinfoRouter = require('./router/userinfo');
+app.use('/my', userinfoRouter)
+
+
 
 // TODO:
 
--- api-server/router/user.js
+++ api-server/router/user.js
@@ -8,6 +8,24 @@ const router = express.Router();
 // 导入用户路由处理函数处理模块
 const userHandler = require('../router_handler/user')
 
+// 导入验证表单数据的中间件
+const userHandler = require('../router_handler/user')
+
+// 导入需要验证的规则对象
+const { reg_login_schema } = require('../schame/user')
+
+// 登录
+router.post('/login', expressJoi(reg_login_schema), userHandler.login)
+
+module.express = router
+
+
+
+
+
+
+
+
 
 
 // 注册新用户
